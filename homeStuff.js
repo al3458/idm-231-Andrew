7fcd4fe38533d15d38e51f14f52416502f5cec7e
Andrew Lenehan <al3458@drexel.edu>
2021-03-05 14:47:48 -0500

    updates and effects
    Updates some of the images and added hover effects as well as added things for the future code

---
+++ homeStuff.js
@@ -0,0 +1,37 @@
+const wandaObj= document.getElementById('wanda');
+const visionObj= document.getElementById('vision');
+const captainAmericaObj= document.getElementById('captainAmerica');
+const ironManObj= document.getElementById('ironMan');
+const doctorStrangeObj= document.getElementById('doctorStrange');
+const blackWidowObj= document.getElementById('blackWidow');
+const spidermanObj= document.getElementById('spiderMan');
+const hulkObj= document.getElementById('hulk');
+const thorObj= document.getElementById('thor');
+const hawkeyeObj= document.getElementById('hawkeye');
+const blackPantherObj= document.getElementById('blackPanther');
+const starLordObj= document.getElementById('starLord');
+
+
+wandaObj.addEventListener('click', function(evt){console.log('wanda clicked on');});
+
+visionObj.addEventListener('click', function(){console.log('vis clicked on');});
+
+captainAmericaObj.addEventListener('click', function(){console.log('cap clicked on');});
+
+ironManObj.addEventListener('click', function(){console.log('iron man clicked on');});
+
+doctorStrangeObj.addEventListener('click', function(){console.log('doctor clicked on');});
+
+blackWidowObj.addEventListener('click', function(){console.log('nat clicked on');});
+
+spidermanObj.addEventListener('click', function(){console.log('spiderman clicked on');});
+
+hulkObj.addEventListener('click', function(){console.log('hulk clicked on');});
+
+thorObj.addEventListener('click', function(){console.log('thor clicked on');});
+
+hawkeyeObj.addEventListener('click', function(){console.log('clint clicked on');});
+
+blackPantherObj.addEventListener('click', function(){console.log('tachala clicked on');});
+
+starLordObj.addEventListener('click', function(){console.log('star lord clicked on');});
